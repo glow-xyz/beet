17bc486265a3af872de66e26b7af77be13adbb62
Stanisław Chmiela <sjchmiela@gmail.com>
2022-07-22 21:06:26 +0200

    Prettify all the files

--- src/beets/enums.ts
+++ src/beets/enums.ts
@@ -1,4 +1,4 @@
-import { Buffer } from 'buffer'
+import { Buffer } from "buffer";
 import {
   BEET_PACKAGE,
   BEET_TYPE_ARG_INNER,
@@ -7,17 +7,17 @@ import {
   FixedSizeBeet,
   isFixedSizeBeet,
   SupportedTypeDefinition,
-} from '../types'
-import { u8 } from './numbers'
-import { isBeetStruct } from '../struct'
-import { isFixableBeetStruct } from '../struct.fixable'
+} from "../types";
+import { u8 } from "./numbers";
+import { isBeetStruct } from "../struct";
+import { isFixableBeetStruct } from "../struct.fixable";
 
 // -----------------
 // Fixed Scalar Enum
 // -----------------
 
 function resolveEnumVariant<T>(value: T, isNumVariant: boolean): keyof Enum<T> {
-  return (isNumVariant ? `${value}` : value) as keyof Enum<T>
+  return (isNumVariant ? `${value}` : value) as keyof Enum<T>;
 }
 
 /**
@@ -30,46 +30,46 @@ function resolveEnumVariant<T>(value: T, isNumVariant: boolean): keyof Enum<T> {
 export function fixedScalarEnum<T>(
   enumType: Enum<T>
 ): FixedSizeBeet<Enum<T>, Enum<T>> {
-  const keys = Object.keys(enumType)
+  const keys = Object.keys(enumType);
   return {
     write(buf: Buffer, offset: number, value: T) {
-      const isNumVariant = typeof value === 'number'
-      const variantKey = resolveEnumVariant(value, isNumVariant)
+      const isNumVariant = typeof value === "number";
+      const variantKey = resolveEnumVariant(value, isNumVariant);
 
       if (!keys.includes(variantKey)) {
         throw new Error(
           `${value} should be a variant of the provided enum type, i.e. [ ${Object.values(
             enumType
-          ).join(', ')} ], but isn't`
-        )
+          ).join(", ")} ], but isn't`
+        );
       }
 
       if (isNumVariant) {
-        u8.write(buf, offset, value)
+        u8.write(buf, offset, value);
       } else {
-        const enumValue = enumType[variantKey] as number
-        u8.write(buf, offset, enumValue)
+        const enumValue = enumType[variantKey] as number;
+        u8.write(buf, offset, enumValue);
       }
     },
 
     read(buf: Buffer, offset: number): T {
-      const value = u8.read(buf, offset) as T | number
-      const isNumVariant = typeof value === 'number'
-      const variantKey = resolveEnumVariant(value, isNumVariant)
+      const value = u8.read(buf, offset) as T | number;
+      const isNumVariant = typeof value === "number";
+      const variantKey = resolveEnumVariant(value, isNumVariant);
 
       if (!keys.includes(variantKey)) {
         throw new Error(
           `${value} should be a of a variant of the provided enum type, i.e. [ ${Object.values(
             enumType
-          ).join(', ')} ], but isn't`
-        )
+          ).join(", ")} ], but isn't`
+        );
       }
-      return (isNumVariant ? value : enumType[variantKey]) as T
+      return (isNumVariant ? value : enumType[variantKey]) as T;
     },
 
     byteSize: u8.byteSize,
-    description: 'Enum',
-  }
+    description: "Enum",
+  };
 }
 
 // -----------------
@@ -85,7 +85,7 @@ export function fixedScalarEnum<T>(
  *
  * @category beet/composite
  */
-export type UniformDataEnum<Kind, Data> = { kind: Kind & number; data: Data }
+export type UniformDataEnum<Kind, Data> = { kind: Kind & number; data: Data };
 /**
  * De/Serializes an {@link Enum} that contains a type of data, i.e. a {@link Struct}.
  * The main difference to a Rust enum is that the type of data has to be the
@@ -106,26 +106,26 @@ export function uniformDataEnum<Kind, Data>(
       offset: number,
       value: UniformDataEnum<Kind, Data>
     ) {
-      u8.write(buf, offset, value.kind)
-      inner.write(buf, offset + 1, value.data)
+      u8.write(buf, offset, value.kind);
+      inner.write(buf, offset + 1, value.data);
     },
 
     read: function (buf: Buffer, offset: number): UniformDataEnum<Kind, Data> {
-      const kind = u8.read(buf, offset) as UniformDataEnum<Kind, Data>['kind']
-      const data = inner.read(buf, offset + 1)
-      return { kind, data }
+      const kind = u8.read(buf, offset) as UniformDataEnum<Kind, Data>["kind"];
+      const data = inner.read(buf, offset + 1);
+      return { kind, data };
     },
     byteSize: 1 + inner.byteSize,
     description: `UniformDataEnum<${inner.description}>`,
-  }
+  };
 }
 
 // -----------------
 // Data Enum
 // -----------------
 type EnumDataVariant<Kind, Data> = {
-  __kind: Kind
-} & Data
+  __kind: Kind;
+} & Data;
 
 function enumDataVariantBeet<Kind, T>(
   inner: FixedSizeBeet<T>,
@@ -134,18 +134,18 @@ function enumDataVariantBeet<Kind, T>(
 ): FixedSizeBeet<EnumDataVariant<Kind, T>> {
   return {
     write(buf: Buffer, offset: number, value: T) {
-      u8.write(buf, offset, discriminant)
-      inner.write(buf, offset + u8.byteSize, value)
+      u8.write(buf, offset, discriminant);
+      inner.write(buf, offset + u8.byteSize, value);
     },
 
     read(buf: Buffer, offset: number) {
-      const val: T = inner.read(buf, offset + u8.byteSize)
-      return { __kind: kind, ...val }
+      const val: T = inner.read(buf, offset + u8.byteSize);
+      return { __kind: kind, ...val };
     },
 
     byteSize: inner.byteSize + u8.byteSize,
     description: `EnumData<${inner.description}>`,
-  }
+  };
 }
 
 /**
@@ -177,77 +177,77 @@ export function dataEnum<T, Key extends keyof T = keyof T>(
 ) {
   for (const [_, beet] of variants) {
     // NOTE: tried to enforce this with types but failed to do so for now
-    const isStruct = isBeetStruct(beet) || isFixableBeetStruct(beet)
+    const isStruct = isBeetStruct(beet) || isFixableBeetStruct(beet);
     if (!isStruct) {
-      throw new Error('dataEnum: data beet must be a struct')
+      throw new Error("dataEnum: data beet must be a struct");
     }
   }
 
   return {
     toFixedFromData(buf: Buffer, offset: number) {
-      const discriminant = u8.read(buf, offset)
-      const variant = variants[discriminant]
+      const discriminant = u8.read(buf, offset);
+      const variant = variants[discriminant];
       if (variant == null) {
         throw new Error(
           `Discriminant ${discriminant} out of range for ${variants.length} variants`
-        )
+        );
       }
-      const [__kind, dataBeet] = variant
+      const [__kind, dataBeet] = variant;
       const fixed = isFixedSizeBeet(dataBeet)
         ? dataBeet
-        : dataBeet.toFixedFromData(buf, offset + 1)
+        : dataBeet.toFixedFromData(buf, offset + 1);
 
-      return enumDataVariantBeet(fixed, discriminant, __kind)
+      return enumDataVariantBeet(fixed, discriminant, __kind);
     },
 
     toFixedFromValue(val: any) {
       if (val.__kind == null) {
-        const keys = Object.keys(val).join(', ')
-        const validKinds = variants.map(([__kind]) => __kind).join(', ')
+        const keys = Object.keys(val).join(", ");
+        const validKinds = variants.map(([__kind]) => __kind).join(", ");
         throw new Error(
           `Value with fields [ ${keys} ] is missing __kind, ` +
             `which needs to be set to one of [ ${validKinds} ]`
-        )
+        );
       }
 
       const discriminant = variants.findIndex(
         ([__kind]) => __kind === val.__kind
-      )
+      );
       if (discriminant < 0) {
-        const validKinds = variants.map(([__kind]) => __kind).join(', ')
+        const validKinds = variants.map(([__kind]) => __kind).join(", ");
         throw new Error(
           `${val.__kind} is not a valid kind, needs to be one of [ ${validKinds} ]`
-        )
+        );
       }
-      const variant = variants[discriminant]
+      const variant = variants[discriminant];
 
-      const { __kind, ...dataValue } = val
-      const [__variantKind, dataBeet] = variant
+      const { __kind, ...dataValue } = val;
+      const [__variantKind, dataBeet] = variant;
       const fixed = isFixedSizeBeet(dataBeet)
         ? dataBeet
-        : dataBeet.toFixedFromValue(dataValue)
-      return enumDataVariantBeet(fixed, discriminant, __variantKind)
+        : dataBeet.toFixedFromValue(dataValue);
+      return enumDataVariantBeet(fixed, discriminant, __variantKind);
     },
 
     description: `DataEnum<${variants.length} variants>`,
-  }
+  };
 }
 
 /**
  * @category TypeDefinition
  */
-export type EnumsExports = keyof typeof import('./enums')
+export type EnumsExports = keyof typeof import("./enums");
 /**
  * @category TypeDefinition
  */
-export type EnumsTypeMapKey = 'fixedScalarEnum' | 'dataEnum'
+export type EnumsTypeMapKey = "fixedScalarEnum" | "dataEnum";
 /**
  * @category TypeDefinition
  */
 export type EnumsTypeMap = Record<
   EnumsTypeMapKey,
   SupportedTypeDefinition & { beet: EnumsExports }
->
+>;
 
 /**
  * Maps composite beet exports to metadata which describes in which package it
@@ -258,19 +258,19 @@ export type EnumsTypeMap = Record<
  */
 export const enumsTypeMap: EnumsTypeMap = {
   fixedScalarEnum: {
-    beet: 'fixedScalarEnum',
+    beet: "fixedScalarEnum",
     isFixable: false,
     sourcePack: BEET_PACKAGE,
-    ts: '<TypeName>',
+    ts: "<TypeName>",
     arg: BEET_TYPE_ARG_INNER,
     pack: BEET_PACKAGE,
   },
   dataEnum: {
-    beet: 'dataEnum',
+    beet: "dataEnum",
     isFixable: false,
     sourcePack: BEET_PACKAGE,
-    ts: 'DataEnum<Kind, Inner>',
+    ts: "DataEnum<Kind, Inner>",
     arg: BEET_TYPE_ARG_INNER,
     pack: BEET_PACKAGE,
   },
-}
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,38 @@
-import debug from 'debug'
-import colors from 'ansicolors'
+import debug from "debug";
+import colors from "ansicolors";
 import {
   FixableBeet,
   FixedSizeBeet,
   isElementCollectionFixedSizeBeet,
   isFixableBeet,
-} from './types'
-const { brightBlack } = colors
+} from "./types";
+const { brightBlack } = colors;
 
-export const logError = debug('beet:error')
-export const logInfo = debug('beet:info')
-export const logDebug = debug('beet:debug')
-export const logTrace = debug('beet:trace')
+export const logError = debug("beet:error");
+export const logInfo = debug("beet:info");
+export const logDebug = debug("beet:debug");
+export const logTrace = debug("beet:trace");
 
 export function beetBytes<T, V = Partial<T>>(
   beet: FixedSizeBeet<T, V> | FixableBeet<T, V>,
   isFixable = false
 ) {
-  let bytes: string
+  let bytes: string;
   if (isFixableBeet(beet)) {
-    bytes = '? B'
+    bytes = "? B";
   } else if (isElementCollectionFixedSizeBeet(beet)) {
-    const len = isFixable ? 'length' : beet.length
-    const lenBytes = beet.lenPrefixByteSize
+    const len = isFixable ? "length" : beet.length;
+    const lenBytes = beet.lenPrefixByteSize;
     bytes =
       lenBytes > 0
         ? `${lenBytes} + (${beet.elementByteSize} * ${len}) B  (${beet.byteSize} B)`
-        : `(${beet.elementByteSize} * ${len}) B (${beet.byteSize} B)`
+        : `(${beet.elementByteSize} * ${len}) B (${beet.byteSize} B)`;
   } else {
-    bytes = `${beet.byteSize} B`
+    bytes = `${beet.byteSize} B`;
   }
-  return brightBlack(bytes)
+  return brightBlack(bytes);
 }
 
 export function bytes(n: number) {
-  return brightBlack(`${n} B`)
+  return brightBlack(`${n} B`);
 }
